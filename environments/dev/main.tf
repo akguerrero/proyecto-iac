28a0a5d5ad44196d0dd00ddd27a4757fa31a2c9c
Kevin Andres Guerrero Tejeda <kevin.guerrero@vasslatam.com>
2024-02-07 09:57:01 -0500

    lambda-dev

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,11 +8,12 @@ module "OnboardingWeb_Client_Enroll_User" {
   timeout       = 120
   subnet_ids         = ["subnet-068e85c3d2bae6b3c", "subnet-02fda45c0b443c614"]
   security_group_ids = ["sg-0c6f16e6e27a31eeb"]
-  BACKEND_URL          = "https://tbfc01-ic06bk20.towerbank.interno/PA_Towerbank_ICBanking.WebApi.BO/api/Framework/CryptoCustom/Login"
-  ENCRYPT_FUNCTION     = "AppCryp_Access_Security_Encrypt"
-  X_CHANNEL            = "100"
-  X_TWR_TOKEN          = "3F2504E0-4F89-11D3-9A0C-0305E82C3301"
-
+  BACKEND_URL                    = "https://tbfc01-ic06bk20.towerbank.interno/PA_Towerbank_ICBanking.WebApi.BO/api/framework/cryptocustom/UserEnroll"
+  X_COMPRESSED_COMMUNICATION     = false
+  X_CHANNEL                      = 100
+  X_PROTECTED_COMMUNICATION      = false
+  X_SESSION_EXPIRATION_TIME      = 0
+  X_TWR_TOKEN                    = "3F2504E0-4F89-11D3-9A0C-0305E82C3301"
   Nameproject = var.Nameproject
   Environment = var.Environment
 }
@@ -26,10 +27,12 @@ module "OnboardingWeb_Client_Validate_User" {
   timeout       = 120
   subnet_ids         = ["subnet-068e85c3d2bae6b3c", "subnet-02fda45c0b443c614"]
   security_group_ids = ["sg-0c6f16e6e27a31eeb"]
-  BACKEND_URL          = "https://tbfc01-ic06bk20.towerbank.interno/PA_Towerbank_ICBanking.WebApi.BO/api/Framework/CryptoCustom/Login"
-  ENCRYPT_FUNCTION     = "AppCryp_Access_Security_Encrypt"
-  X_CHANNEL            = "10000"
-  X_TWR_TOKEN          = "3F2504E0-4F89-11D3-9A0C-0305E82C3301"
+  BACKEND_URL                    = "https://tbfc01-ic06bk20.towerbank.interno/PA_Towerbank_ICBanking.WebApi.BO/api/framework/cryptocustom/validateuserdata"
+  X_COMPRESSED_COMMUNICATION     = false
+  X_CHANNEL                      = 100
+  X_PROTECTED_COMMUNICATION      = false
+  X_SESSION_EXPIRATION_TIME      = 0
+  X_TWR_TOKEN                    = "3F2504E0-4F89-11D3-9A0C-0305E82C3301"
   Nameproject = var.Nameproject
   Environment = var.Environment
 }
