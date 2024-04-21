1baa420ba6fd9dc79471431895d1505cf02a2670
akguerrero <akguerrero@uninorte.edu.co>
2024-04-21 13:02:32 -0500

    .

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,11 +1,5 @@
-#--------------------------------------------------------------
-# Este modulo crea los recursos necesarios para crear Lambda
-# Lambda python
-#--------------------------------------------------------------
-
 resource "aws_lambda_function" "lambda_python" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
+
   filename      = var.filename
   description   = var.description
   function_name = var.function_name
@@ -13,8 +7,6 @@ resource "aws_lambda_function" "lambda_python" {
   handler       = var.handler
   runtime       = var.runtime
   timeout       = var.timeout
-
- #source_code_hash = data.archive_file.lambda.output_base64sha256
 
   vpc_config {
     subnet_ids         = [""]
@@ -32,8 +24,4 @@ resource "aws_lambda_permission" "lambda_permission" {
   action        = "lambda:InvokeFunction"
   function_name = var.function_name
   principal     = "apigateway.amazonaws.com"
-
-  # La parte /*/*/* permite la invocación desde cualquier etapa, método y ruta de recurso
-  # dentro de API Gateway REST API.
-  #source_arn = "arn:aws:execute-api:us-east-1:*:*/*/*/*"
 }
